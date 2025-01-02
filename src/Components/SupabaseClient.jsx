7bf40c0d79edde443ade313591c8e739d403f6ce
CLINTON <issnasty2@gmail.com>
2025-01-02 14:27:48 +0300

    remove keys

--- src/Components/SupabaseClient.jsx
+++ src/Components/SupabaseClient.jsx
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase
-const supabaseUrl = "https://gtevlsbundbddlqqslbf.supabase.co"; // Replace with your Supabase URL
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0ZXZsc2J1bmRiZGRscXFzbGJmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4MDE3MTEsImV4cCI6MjA1MTM3NzcxMX0.K0j3QjXqb-WZ3bOf8jlSDcmjV68OMYKet4_xf52kNXM"; // Replace with your Supabase anonymous key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL; // Accessing the environment variable correctly
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY; // Accessing the environment variable correctly
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
