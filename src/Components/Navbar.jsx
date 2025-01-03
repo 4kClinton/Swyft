eeaf1269be4057e486de53949a99aef14e350a33
CLINTON <issnasty2@gmail.com>
2025-01-04 02:55:48 +0300

    padding  login

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -142,7 +142,7 @@ const Navbar = () => {
               </button>
             ) : (
               <Link onClick={toggleSidebar} to="/login">
-                <button className="login-button" style={{width:"120%",height:"unset"}}>Log In</button>
+                <button className="login-button" style={{width:"120%",height:"unset",fontSize:"large",}}>Log In</button>
               </Link>
             )}
           </div>
