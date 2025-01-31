bdd9a42c4eeacf74507d2ba347cd434b35f3ac85
Brian <brianhilsden@gmail.com>
2025-01-31 20:50:57 +0300

    updated firebase

--- firebase.js
+++ firebase.js
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
-
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration from the .env file
 const firebaseConfig = {
-  apiKey: 'AIzaSyD3UOJEpimIg7F9gGtOF_LdoXQoWc-7rqg',
-  authDomain: 'swyft-ca43e.firebaseapp.com',
-  projectId: 'swyft-ca43e',
-  storageBucket: 'swyft-ca43e.firebasestorage.app',
-  messagingSenderId: '474475564210',
-  appId: '1:474475564210:web:334c733b7f4b6b3d784dab',
-  measurementId: 'G-WFN6CLV1N3',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
