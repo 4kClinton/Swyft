f41c776052f57655a433ef001904a9b649b63926
Clinton Kibet <143574368+4kClinton@users.noreply.github.com>
2025-06-23 16:43:14 +0300

    removed otp from login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -21,14 +21,10 @@ const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const [otp, setOtp] = useState('');
-  const [step, setStep] = useState(1); // 1: Login, 2: OTP Verification
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(null);
   const [loading, setLoading] = useState(false);
   const user = useSelector((state) => state.user.value);
-
-  // Remove Install Prompt states and useEffect from Login
 
   const togglePasswordVisibility = () => {
     setShowPassword((prev) => !prev);
@@ -47,31 +43,6 @@ const Login = () => {
         { headers: { 'Content-Type': 'application/json' } }
       );
 
-      setSuccess(response.data.message || 'OTP sent to your email');
-      setStep(2); // Move to OTP verification step
-    } catch (err) {
-      console.error(err.response);
-      setError(
-        err.response?.data?.error || 'An error occurred. Please try again.'
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const verifyOtp = async (event) => {
-    event.preventDefault();
-    setLoading(true);
-    setError(null);
-    setSuccess(null);
-
-    try {
-      const response = await axios.post(
-        'https://swyft-backend-client-nine.vercel.app/verify-otp',
-        { email, otp },
-        { headers: { 'Content-Type': 'application/json' } }
-      );
-
       const { access_token, user, message } = response.data;
 
       Cookies.set('authTokencl1', access_token, {
@@ -87,10 +58,12 @@ const Login = () => {
 
       setTimeout(() => {
         navigate('/dash');
-      }, 3000);
+      }, 1500);
     } catch (err) {
       console.error(err.response);
-      setError(err.response?.data?.error || 'Invalid OTP. Please try again.');
+      setError(
+        err.response?.data?.error || 'An error occurred. Please try again.'
+      );
     } finally {
       setLoading(false);
     }
@@ -116,78 +89,49 @@ const Login = () => {
           </div>
         )}
 
-        {step === 1 ? (
-          <form onSubmit={logIn}>
+        <form onSubmit={logIn}>
+          <input
+            placeholder="Email"
+            type="email"
+            className="login-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <Box className="input-container">
             <input
-              placeholder="Email"
-              type="email"
+              placeholder="Password"
+              type={showPassword ? 'text' : 'password'}
               className="login-input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <Box className="input-container">
-              <input
-                placeholder="Password"
-                type={showPassword ? 'text' : 'password'}
-                className="login-input"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
+            <IconButton
+              aria-label="toggle password visibility"
+              onClick={togglePasswordVisibility}
+              className="password-toggle"
+            >
+              {showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          </Box>
+          <button
+            type="submit"
+            className="login-button"
+            color="success"
+            disabled={loading}
+          >
+            {loading ? (
+              <CircularProgress
+                className="login-loader"
+                size={34}
+                color="#fff"
               />
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={togglePasswordVisibility}
-                className="password-toggle"
-              >
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </Box>
-            <button
-              type="submit"
-              className="login-button"
-              color="success"
-              disabled={loading}
-            >
-              {loading ? (
-                <CircularProgress
-                  className="login-loader"
-                  size={34}
-                  color="#fff"
-                />
-              ) : (
-                'Log In'
-              )}
-            </button>
-          </form>
-        ) : (
-          <form onSubmit={verifyOtp}>
-            <input
-              placeholder="Enter OTP"
-              type="text"
-              className="login-input"
-              value={otp}
-              onChange={(e) => setOtp(e.target.value)}
-              required
-            />
-            <button
-              type="submit"
-              color="success"
-              disabled={loading}
-              className="login-button"
-            >
-              {loading ? (
-                <CircularProgress
-                  className="login-loader"
-                  size={34}
-                  color="#fff"
-                />
-              ) : (
-                'Verify OTP'
-              )}
-            </button>
-          </form>
-        )}
+            ) : (
+              'Log In'
+            )}
+          </button>
+        </form>
 
         <Button
           onClick={() => navigate('/signup')}
